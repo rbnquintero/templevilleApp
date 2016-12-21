5132f9704af09257ea844a7f3ebf88bee83692dd
Ruben Quintero <rbnquintero@gmail.com>
2016-12-21 17:02:48 -0600

    cambios estructura inicial

--- src/TemplevilleApp.js
+++ src/TemplevilleApp.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
   View
 } from 'react-native';
+
+var MainView = require('./views/MainView')
 
 /* REDUX */
 import type {State as AppReducer} from './reducers/appReducer'
@@ -30,40 +29,10 @@ class TemplevilleApp extends Component {
 
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <MainView style={{flex:1}}/>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 function select(store) {
   return {
